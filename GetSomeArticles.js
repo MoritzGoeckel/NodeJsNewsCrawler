e2681483bfcd5a08887cd41b523bad077beb9ca2
Moritz Göckel <moritz.goeckel@gmail.com>
2017-03-01 15:06:16 +0100

    Worked on the non technical interface

--- GetSomeArticles.js
+++ GetSomeArticles.js
@@ -17,11 +17,61 @@ let request = require('request');
 
 let Express = require('express');
 let ExpressRest = require('express-rest');
+Vibrant = require('node-vibrant');
 
 //End imports
 
+//Todo: Make it reliable (Every word gets its proper article)
+//Todo: Make the page responsive (bootstrap grid)
+//Todo: Generate different tiles (done:1x1 Todo:2x1 1x2 description)
+
 function getToday(){
     return Math.floor(Date.now() / 1000 / 60 / 60 / 24);
+}
+
+function doLink(link, query, rank, articles){
+    request({url: link.url, headers: {'User-Agent': 'request'}}, function(error, response, body) {
+        console.log("Download: " + link.url);
+
+        if(typeof response == 'undefined')
+        {
+            console.log("Error downloading: " + this.url);
+            return;
+        }
+        
+        if (!error && response.statusCode == 200) {
+            let $ = cheerio.load(body);
+
+            let article = {
+                    rank:rank,
+                    link:link,
+                    query:query,
+                    title:$("meta[property='og:title']").attr("content"),
+                    desc:$("meta[property='og:description']").attr("content"),
+                    img:$("meta[property='og:image']").attr("content")
+                };
+            
+            if(article.link != undefined && article.query != undefined && article.title != undefined && article.desc != undefined && article.img != undefined)
+            {
+                console.log("OK");
+
+                if(article.img != undefined)
+                    Vibrant.from(article.img).getPalette((err, palette) => article.palette = palette);
+
+                let found = false;
+                for(let a in articles)
+                {
+                    if(articles[a].link.url == article.link.url){
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(found == false && article.link.title != undefined)
+                    articles.push(article);
+            }
+        }
+    });
 }
 
 let dm = new DataManager(config.redisPort, function()
@@ -31,7 +81,7 @@ let dm = new DataManager(config.redisPort, function()
     let articles = [];
 
     api.getMostPopularWordsOnDay(getToday(), 15, function(popular){
-       for(let i = 0; i < popular.length && i < 30; i++)
+       for(let i = 0; i < popular.length && i < 50; i++)
         {
             api.getSameHeadlineCountForDayAndWord(getToday(), [popular[i].word], function(related){
                 let query = [popular[i].word];
@@ -41,41 +91,7 @@ let dm = new DataManager(config.redisPort, function()
                 api.getLinksToWords(query, function(links){
                     if(links[0] != undefined)
                         api.getLink(links[0], function(link){
-                            request({url: link.url, headers: {'User-Agent': 'request'}}, function(error, response, body) {
-                                if(typeof response == 'undefined')
-                                {
-                                    console.log("Error downloading: " + this.url);
-                                    return;
-                                }
-                                
-                                if(response.statusCode == 302){
-                                    console.log("Moved to: " + response.headers['Location']);
-                                }
-                                
-                                if (!error && response.statusCode == 200) {
-                                    let $ = cheerio.load(body);
-
-                                    let article = {
-                                            link:link,
-                                            query:query,
-                                            title:$("meta[property='og:title']").attr("content"),
-                                            desc:$("meta[property='og:description']").attr("content"),
-                                            img:$("meta[property='og:image']").attr("content")
-                                        };
-                                    
-                                    let found = false;
-                                    for(let a in articles)
-                                    {
-                                        if(articles[a].link.url == article.link.url){
-                                            found = true;
-                                            break;
-                                        }
-                                    }
-
-                                    if(found == false)
-                                        articles.push(article);
-                                }
-                            });
+                            doLink(link, query, i, articles);
                         });
                 });
             });
@@ -89,6 +105,7 @@ let dm = new DataManager(config.redisPort, function()
     exp.use("/", Express.static(__dirname + '/NonTechnicalFrontend'));
 
     rest.get('/api/somearticles/', function(req, rest) {
+        articles.sort(function(a,b) {return (a.rank > b.rank) ? 1 : ((b.rank > a.rank) ? -1 : 0);});
         return rest.ok(articles);
     });
 
--- NonTechnicalFrontend/script.js
+++ NonTechnicalFrontend/script.js
@@ -1,8 +1,56 @@
+let urlTags = [
+    {pattern:"/opinion/", tag:"Opinion"},
+    {pattern:"/us-news", tag:"US"},
+    {pattern:"analysis", tag:"Analysis"}
+    /*{pattern:"/opinion/", tag:"Opinion"},
+    {pattern:"/opinion/", tag:"Opinion"},
+    {pattern:"/opinion/", tag:"Opinion"},
+    {pattern:"/opinion/", tag:"Opinion"},*/
+]; 
+
+function getRGB(color)
+{
+    return "rgba(" + color.rgb[0] + "," + color.rgb[1] + "," + color.rgb[2] + ", 0.8)";
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getTag(article){
+    let urlTag = findKeyWord(urlTags, article.link.url);
+    let keywordTag = capitalizeFirstLetter(article.query[0]);
+    return keywordTag;   
+}
+
+function findKeyWord(words, text){
+    for(let i in words){
+        if(text.indexOf(words[i].pattern) != -1)
+            return words[i].tag;
+    }
+}
+
 $.getJSON( "api/somearticles", function( articles ) {
     for(let a in articles){
-        let obj = $("<a href='"+articles[a].link.url+"'><div class='article'><span>"+articles[a].title+"</span></div></a>");
-        obj.children(":first").css("backgroundImage", "url('"+articles[a].img+"')");
 
-        $("#wrapper").append(obj);
+        if(articles[a].link.title.length < 200){
+            let foundTag = getTag(articles[a]);
+
+            let tag = foundTag != undefined ? "<span class='tag'>" + foundTag + ": </span>" : "";
+
+            let obj = $("<a href='" + articles[a].link.url + "'><div class='article'><span class='headline'>" + tag + "<span class='title'>" + articles[a].link.title + "</span></span></div></a>");
+            obj.children(":first").css("backgroundImage", "url('" + articles[a].img + "')");
+
+            if(articles[a].palette.LightVibrant != null){
+                //obj.children(":first").children(":first").css("color", getRGB(articles[a].palette.LightVibrant));
+                obj.children(":first").children(".headline").css("background-color", getRGB(articles[a].palette.DarkVibrant));
+                
+                obj.hover(function(e) { 
+                    $(this).children(":first").children(".headline").css("background-color", e.type === "mouseenter" ? "rgba(255, 242, 0, 0.8)":getRGB(articles[a].palette.DarkVibrant)); 
+                });
+            }
+
+            $("#wrapper").append(obj);
+        }
     }
 });
